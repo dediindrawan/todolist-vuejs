100ffb890beeab5f50bd7a57503216f9c20ee2c7
Dedi Indrawan <107289320+dediindrawan@users.noreply.github.com>
2023-06-14 23:17:22 +0700

    mengubah beberapa tampilan

--- app.vue
+++ app.vue
@@ -1,32 +1,47 @@
 <template>
-  <div class="container-fluid">
-    <h1 v-html="rawHtml"></h1>
-    <p v-if="!isCreating && tasks.length == 1">Belum ada task</p>
-    <p v-else-if="!titleValue && !descriptionValue">Buat task baru</p>
+  <div class="item-task m-4">
+    <!-- task group container start -->
+    <div class="container m-auto">
+      <!-- title and notif -->
+      <h1 v-html="rawHtml" class="mb-4"></h1>
+      <p class="text-muted" v-if="isCreating && tasks.length == 1">{{ notif }}</p>
+      <p class="text-muted" v-else-if="!isCreating && tasks.length == 1">Buat task baru</p>
 
-    <div v-for="task of tasks" :key="task.id" class="task-list d-flex align-items-center mb-3">
-      <input v-if="task.title || task.description" :checked="tasks[0].isDone" @change="checkTask(task)" type="checkbox" name="status" />
-
-      <div class="list-wrapper col-md-12 ml-3">
-        <div :class="{ 'task-title': true, completed: task.isDone }">
-          <strong>{{ task.title }}</strong>
+      <!-- task list -->
+      <div v-for="task of tasks" :key="task.id" class="task-list">
+        <div class="wrapper d-flex p-2" v-if="task.title && task.description">
+          <input type="checkbox" name="status" id="task" class="mr-3" :checked="tasks.isDone" v-if="task.title && task.description" @change="checkTask(task.id)" />
+          <div class="d-flex flex-column">
+            <div :class="{ 'task-title': true, isCompleted: task.isDone }">
+              {{ task.title }}
+            </div>
+            <div class="task-description small text-muted">
+              {{ task.description }}
+            </div>
+            <a href="#" class="delete-button text-danger" v-if="task.title && task.description" @click="deleteTask(task.id)">Delete Task</a>
+          </div>
         </div>
-        <div class="task-description">{{ task.description }}</div>
-        <a href="#" @click="deleteTask(task.id)" v-if="task.title || task.description">Delete</a>
+      </div>
+
+      <!-- action control areas -->
+      <div class="action d-flex justify-content-between">
+        <a href="#" class="add-button" v-if="isCreating" @click="isCreating = !isCreating">Add Task</a>
+
+        <div class="add-card col-md-8 px-0" v-else>
+          <div class="card border-0 mb-2">
+            <div class="card-body d-flex flex-column p-0">
+              <input v-model="titleValue" class="form-control border-1 mb-2" placeholder="Task Title" type="text" />
+              <textarea v-model="descriptionValue" class="form-control border-1 small" placeholder="Task Description" rows="3"></textarea>
+            </div>
+          </div>
+          <div class="button-wrapper d-flex">
+            <button class="btn btn-primary me-2 mr-2 col-sm-2" @click="saveTask">Save</button>
+            <button class="btn btn-outline-secondary col-sm-2" @click="isCreating = !isCreating">Cancel</button>
+          </div>
+        </div>
       </div>
     </div>
-
-    <div class="action-control">
-      <a href="#" v-if="!isCreating" @click="isCreating = !isCreating">Add Task</a>
-
-      <div v-else class="task-form">
-        <input v-model="titleValue" class="col-md-12 mb-3" type="text" name="Title" placeholder="Title" />
-        <textarea v-model="descriptionValue" class="col-md-12 mb-3" name="Description" placeholder="Description"></textarea>
-
-        <button @click="saveTask" class="btn btn-primary mr-3 col-sm-2">Save</button>
-        <button @click="isCreating = !isCreating" class="btn btn-warning col-sm-2">Cancel</button>
-      </div>
-    </div>
+    <!-- task group container end -->
   </div>
 </template>
 
@@ -36,7 +51,9 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 export default {
   data() {
     return {
-      rawHtml: 'Todo List',
+      // userface default display
+      rawHtml: `Todo List <small style="font-size: 1rem;color:grey">WebApplication</small>`,
+      notif: 'Belum ada task',
       tasks: [
         {
           isDone: false,
@@ -48,33 +65,45 @@ export default {
     };
   },
   methods: {
+    // create task
     saveTask() {
       this.tasks.unshift({
         id: this.tasks.length + 1,
         title: this.titleValue,
         description: this.descriptionValue,
+        isDone: false,
       });
     },
     deleteTask(index) {
       const taskIndex = this.tasks.findIndex((task) => task.id === index);
+
       if (taskIndex !== -1) {
         this.tasks.splice(taskIndex, 1);
       }
     },
-    checkTask(task) {
-      task.isDone = !task.isDone;
+    checkTask(index) {
+      const taskIndex = this.tasks.findIndex((task) => task.id === index);
+
+      if (taskIndex !== -1) {
+        this.tasks[taskIndex].isDone = !this.tasks[taskIndex].isDone;
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-.completed {
-  text-decoration: line-through;
-  font-style: italic;
-  color: grey;
+.task-list {
+  margin: 0.75em auto;
+  border-radius: 5px;
+  background: linear-gradient(to bottom right, rgba(0, 85, 255, 0.1), rgba(0, 116, 224, 0.2));
 }
-.list-wrapper {
-  background-color: rgba(102, 104, 109, 0.1);
+textarea {
+  resize: none;
+}
+.isCompleted {
+  font-style: italic;
+  text-decoration: line-through;
+  opacity: 0.75;
 }
 </style>
